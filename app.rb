c86a64088af0164a6b9d87e0308959dcc4456c14
Fronx <fronx@soundcloud.com>
2011-06-17 22:47:39 +0200

    some server-side junk

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+PUBLIC_DIR = File.dirname(__FILE__) + '/public'
+set :public, PUBLIC_DIR
+
+client = Faye::Client.new('http://localhost:9292/faye')
+
+get('/') do
+  File.read(PUBLIC_DIR + '/index.html')
+end
+
+get('/scrobbles') do
+  client.publish('/scrobbles/*',
+    :user => 'sinatra',
+    :song => 'current track'
+  )
+  'yo'
+end
